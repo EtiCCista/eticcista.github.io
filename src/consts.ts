949b2773dce14f7b0cb84a8e60ab90b638e1b842
Ana <carolinadias4567@gmail.com>
2025-07-21 12:17:49 -0300

    Base

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'EtiCCista';
+export const SITE_DESCRIPTION = 'Projeto de TCC de Ana Carolina Sousa Dias.';
