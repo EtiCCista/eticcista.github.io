725f6306f531331113317db69a47f06aefa16986
Ana <carolinadias4567@gmail.com>
2025-08-19 17:56:26 -0300

    Deleta tema

--- src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'EtiCCista';
-export const SITE_DESCRIPTION = 'Projeto de TCC de Ana Carolina Sousa Dias.';
